491f798c1faa097b99462ecc61a27e4000294c8e
Ana Oliveira <aclarafernandes123@gmail.com>
2024-02-21 12:30:42 -0300

    COmmit

--- App.js
+++ App.js
@@ -4,17 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={[styles.numero, {color:'blue'}]}>02</Text>
+      <Text style={[styles.sala, {backgroundColor:'pink'}]}>7</Text>
+      <Text style={[styles.texto, {color:'blue'}]}>Ana Clara Fernandes de Oliveira</Text>
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+   fontSize: 30, 
+   fontWeight: 'bold',
+   alignItems: 'center',
+   justifyContent: 'center',
+   left: '30%',
+  },
+  numero: {
+    fontSize: 25, 
+    fontStyle: 'italic',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: '30%',
+    top: '25%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+   },
+   sala:{
+   fontSize: 20,
+   }
 });
